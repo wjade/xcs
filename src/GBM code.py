91ee1653827706c474376fff89ad7791f0965f94
jygenius <72360405+jygenius@users.noreply.github.com>
2021-05-28 15:05:39 -0400

    Update GBM code.py

--- src/GBM code.py
+++ src/GBM code.py
@@ -10,7 +10,7 @@ import collections
 from collections import namedtuple
 
 #executing data cleaning code
-#exec(open("/Users/yejiang/Desktop/Stanford ML class/project/code/data cleaning.py").read())
+exec(open("/Users/yejiang/Desktop/Stanford ML class/project/code/data cleaning.py").read())
 
 #using default parameters as baseline model
 model_baseline = XGBClassifier(base_score=0.5
@@ -55,9 +55,9 @@ grid_search = []
 i = 0
 
 for col_sample in (0.7, 1):#baseline is 1
-    for row_sample in (0.5, 0.9): #baseline is 0.9
-        for g in (0, 5): #baseline is 0
-            for eta in (0.05, 0.3): #baseline is 0.3
+    for row_sample in (0.5, 0.9, 1): #baseline is 0.9
+        for g in (0, 5, 10): #baseline is 0
+            for eta in (0.05, 0.1, 0.3): #baseline is 0.3
                 for d in (4, 6): #baseline is 6
                     for c in (1, 5): #baseline is 1
 
@@ -104,8 +104,10 @@ print(grid_search)
 max_value = grid_search[grid_search[:,7]==grid_search[:,7].max()]
 
 #max value1
-#[0.7, 0.9, 5, 0.3, 6, 5, 0.9622, 0.9585]
-#[1,    0.9, 5, 0.3, 6, 5, 0.9622, 0.9585]
+#[0.7, 0.5, 5, 0.3, 6, 5, 0.9622, 0.9585]
+#[0.7, 1, 0, 0.3, 6, 5, 0.9634, 0.9585]
+#[1, 0.5, 5, 0.3, 6, 5, 0.9606, 0.9585]
+#[1, 1, 0, 0.3, 6, 5, 0.9634, 0.9585]
 
 #grid search's result is slightly better than baseline, imporved from 0.9578 to 0.9585
 #comparing the parameters, the baseline model appears to be overfitting(parameters are telling the same story)
